23400921d2ea62658375cd4008addcc242803703
vismaya01 <kvvismaya98@gmail.com>
2020-11-17 23:02:47 +0530

    Added: To edit existing contact in address book

--- AddressBook.java
+++ AddressBook.java
@@ -3,7 +3,9 @@ import java.util.Scanner;
 public class AddressBook {
 
 	Scanner scanner = new Scanner (System.in);
+	PersonInfo person;
 
+	// add new person
 	public void addPerson() {
 		System.out.print("Enter first name: ");
 		String firstName = scanner.next();
@@ -23,14 +25,45 @@ public class AddressBook {
                 String email = scanner.next();
 
 		//construt new person object
-		PersonInfo person = new PersonInfo(firstName, lastName, address, city, state, zip, phoneNumber, email);
+		person = new PersonInfo(firstName, lastName, address, city, state, zip, phoneNumber, email);
 		System.out.println(person);
 	}
 
+	//edit person details
+	public void editPerson(String firstName, String lastName) {
+               	if (firstName.equals (person.getFirstName()) && lastName.equals (person.getLastName())) {
+			System.out.print("Enter address: ");
+			String address = scanner.next();
+			System.out.print("Enter city: ");
+       			String city = scanner.next();
+			System.out.println("Enter state: ");
+		        String state = scanner.next();
+               		System.out.println("Enter ZIP: ");
+		        String zip = scanner.next();
+			System.out.println("Enter phone number: ");
+			String phoneNumber = scanner.next();
+			System.out.println("Enter email id: ");
+       		        String email = scanner.next();
+
+			person = new PersonInfo(person.getFirstName(),person.getLastName(), address, city, state, zip, phoneNumber, email);
+		}
+		System.out.println("Contact updated successfully\n");
+		System.out.println(person);
+	}
+
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to Address Book");
+		Scanner input = new Scanner (System.in);
+		String firstName, lastName ;
+
 		AddressBook address = new AddressBook();
 		address.addPerson();
+
+		System.out.print("Enter first name and last name of the person to edit the contact: ");
+		firstName=input.next();
+		lastName=input.next();
+		address.editPerson(firstName,lastName);
 	}
 }
 
